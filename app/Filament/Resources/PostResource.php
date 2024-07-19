ceb7209d092e6c9000fd3c60ccb92f22a154b084
Werner Strydom <werners@kishanconsulting.co.za>
2024-07-19 17:30:53 +0200

    Added project to Repo

---
+++ app/Filament/Resources/PostResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PostResource\Pages;
+use App\Filament\Resources\PostResource\RelationManagers;
+use App\Filament\Resources\PostResource\RelationManagers\CommentsRelationManager;
+use App\Models\Post;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make(name:"title")->required()->columnSpanFull(),
+                Forms\Components\RichEditor::make(name:"content")->required()->columnSpanFull(),
+            ]);
+    }
+    //Table Columns: Display title, user name, published_at, and the number of comments in the posts table.
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make(name:"id")->sortable(),
+                Tables\Columns\TextColumn::make(name:"title"),
+                Tables\Columns\TextColumn::make(name:"content"),
+                Tables\Columns\TextColumn::make(name:"created_at"),
+            ])
+            ->defaultSort(column:'id',direction:'desc')
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            CommentsRelationManager::class,
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }
+}
